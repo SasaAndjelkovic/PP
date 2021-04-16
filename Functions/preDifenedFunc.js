93da61bf1fcdc150a7fdc9f8ae4c94c98bb53e74
SasaAndjelkovic <sandjelkovickg@gmail.com>
2021-04-16 20:16:25 +0200

    double git commit -m

--- Functions/preDifenedFunc.js
+++ Functions/preDifenedFunc.js
@@ -16,22 +16,80 @@ function convertInNumbersArray (array) {
 
 console.log (convertInNumbersArray (["1", "21", undefined, "42", "1e+3", Infinity]));
 
-/*Write a program to join all elements of the array into a string skipping elements that are undefined, null, NaN or Infinity.
+/*2. Write a program to join all elements of the array into a string skipping elements that are undefined, null, NaN or Infinity.
 
 	Input: [NaN, 0, 15, false, -22, '', undefined, 47, null]
-	Output: “015false-2247”
+	Output: “015false-2247”*/
 
-Write a program to filter out falsy values from the array.
+function skippingNElements (array) {
+    var string = '';
+    for (var i = 0; i < array.length; i++) {
+        if (isFinite(array[i]) && array[i] !== null) {
+            string += array[i];
+        }
+    }
+    return string;
+}
+
+console.log (skippingNElements ([NaN, 0, 15, false, -22, '', undefined, 47, null]));
+
+/*3. Write a program to filter out falsy values from the array.
 
 	Input: [NaN, 0, 15, false, -22, '', undefined, 47, null]
-Output: [15, -22, 47]
+Output: [15, -22, 47]*/
 
-Write a program that calculates a number of integer values in the array.
+function filterFalsy (array) {
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+        //if (!array[i] === false) { i am not sure what is happening... Now i know :)
+        if (array[i]) {
+            newArray[newArray.length] = array [i];
+        }
+    } 
+    return newArray;
+}
+
+console.log (filterFalsy([NaN, 0, 15, false, -22, '', undefined, 47, null]));
+
+/*4. Write a program that calculates a number of integer values in the array.
 
 	Input: [NaN, 23.1, 15, false, -22.5, '', 4, 7, null]
-Output: 3
+Output: 3*/
 
-Write a program that calculates a number of float values in the array.
+function numberIntegerValue (array) {
+    var calculator = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (parseInt(array[i]) == array[i]) {
+            calculator ++;
+        }
+    }
+    return calculator;
+}
+
+console.log (numberIntegerValue([NaN, 23.1, 15, false, -22.5, '', 4, 7, null]));
+
+/* 5. Write a program that calculates a number of float values in the array.
 
 	Input: [NaN, 23.1, 15, false, -22.5, '', 4, 7, null]
 Output: 2*/
+
+function numberFloatValues (array) {
+    var calculator = 0;
+    for (var i = 0; i < array.length; i++) {
+        if (parseInt(array[i]) !== array[i] /*????*/ && !!array[i] ) //Milos Kuzmic
+        calculator ++;
+    }
+    return calculator;
+}
+
+console.log (numberFloatValues ([NaN, 23.1, 15, false, -22.5, '', 4, 7, null]));
+
+//what is !! --- this is fantastic idea of Mirko Nadj
+
+console.log (0); // => 0
+console.log (!0); // => true
+console.log (!!0); // => false
+
+console.log (1); // => 0
+console.log (!1); // => false
+console.log (!!1); // => true
